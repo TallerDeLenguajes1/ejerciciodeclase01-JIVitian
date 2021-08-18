237612af0a600f9f685a2fc72207cf333d4ed9ad
JIVitian <jivitian@gmail.com>
2021-08-18 17:57:40 -0300

    Adivina el numero!

---
+++ AdivinarElNumero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AdivinarElNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = new Random().Next(0, 101);
+            int userNumber;
+
+            Console.WriteLine("Adivine el numero!");
+
+            do
+            {
+                userNumber = Int32.Parse(Console.ReadLine());
+
+                if(userNumber < number)
+                    Console.WriteLine("El numero es mas grande!");
+                else if (userNumber > number)
+                    Console.WriteLine("El numero es mas chico!");
+                else
+                {
+                    Console.WriteLine("Adivinaste el numero!");
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
