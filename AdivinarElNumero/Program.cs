ea9d1f4cfc44b68a984e4a3a3da5ab8b4c85961b
JIVitian <jivitian@gmail.com>
2021-08-18 18:09:03 -0300

    Con el try catch

--- AdivinarElNumero/Program.cs
+++ AdivinarElNumero/Program.cs
@@ -9,22 +9,36 @@ namespace AdivinarElNumero
             int number = new Random().Next(0, 101);
             int userNumber;
 
-            Console.WriteLine("Adivine el numero!");
+            Console.WriteLine("Adivina el numero!");
 
             do
             {
-                userNumber = Int32.Parse(Console.ReadLine());
+                userNumber = LeerNumero();
 
                 if(userNumber < number)
                     Console.WriteLine("El numero es mas grande!");
                 else if (userNumber > number)
                     Console.WriteLine("El numero es mas chico!");
                 else
-                {
+                {   
                     Console.WriteLine("Adivinaste el numero!");
                     break;
                 }
             } while (true);
+        }
+
+        public static int LeerNumero()
+        {
+            try
+            {
+                return Int32.Parse(Console.ReadLine());
+            }
+            // No controlé que tipo de exepción tira
+            catch (Exception)
+            {
+                Console.WriteLine("Debe ingresar un numero!");
+                return LeerNumero();
+            }
         }
     }
 }
